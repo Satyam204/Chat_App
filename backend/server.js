5344afc604dfcd5c8d391137645d9ac8e362dc51
Satyam204 <satyamshivam366@gmail.com>
2024-05-22 01:07:27 +0530

    connecting frontend with backend using proxy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const { chats } = require("./data");
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, console.log("running"));
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send(chats);
+});
+
+app.listen(PORT, console.log("running"));
