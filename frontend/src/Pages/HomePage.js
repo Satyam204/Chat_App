f65c5251e7701a01eae24c4b77a47b79d37d2bdf
Satyam204 <satyamshivam366@gmail.com>
2024-05-25 01:12:17 +0530

    Creating Homepage and installing Chakra-ui

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Box, Container, Text, Tab,Tabs
+  ,TabList,TabPanel,TabPanels } from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>{`hello ${2 + 3}`}</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="20px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" justifyContent="center">
+          Welcome To Chatter
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="enclosed">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up!</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
