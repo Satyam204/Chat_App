f3a4124f1a81e2cac8cad0681cae9ecf31c28750
Satyam204 <satyamshivam366@gmail.com>
2024-05-31 21:56:15 +0530

    SignUp and photo-Upload(Cloudinary) functionality completed

--- frontend/src/components/Authentication/SignUp.js
+++ frontend/src/components/Authentication/SignUp.js
@@ -1,70 +1,211 @@
-import {Button, FormControl, FormLabel, Input, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SignUp = () => {
+const Signup = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const toast = useToast();
+  //const navigate = useNavigate();
 
-const [name,setName]=useState();
-const [email,setEmail]=useState();
-const [password,setPassword]=useState();
-const [confirmpassword,setConfirmpassword]=useState();
-const [pic,setPic]=useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [password, setPassword] = useState();
+  const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
 
-const postDetails = (pic)=>{alert("uploaded")}
-const submitHandler =()=>{}
+  const submitHandler = async () => {
+    setPicLoading(true);
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+   // console.log(name, email, password, pic);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const response = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      const { data } = response;
+      if (data) {
+        console.log(data);
+        toast({
+          title: "Registration Successful",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setPicLoading(false);
+        setTimeout(() => {
+            window.location.reload(); // Refresh the page after 4 seconds
+          }, 2800);
+      } else {
+        throw new Error("Invalid response data");
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      toast({
+        title: "Error Occurred!",
+        description: error.response?.data?.message || error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+    }
+  };
+
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    console.log(pics);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset",process.env.PRESET);
+      data.append("cloud_name", process.env.CLOUD_NAME);
+      fetch(process.env.CLOUD_URI, {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setPicLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setPicLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
 
   return (
-    <div>
-     <VStack>
-        <FormControl id='first-name' isRequired="true">
-            <FormLabel>Name</FormLabel>
-            <Input 
-                placeholder='Your Name?'
-                onChange={(e)=>{setName(e.target.value)}}
-            />
-        </FormControl>
-        <FormControl id='email' isRequired="true">
-            <FormLabel>Email</FormLabel>
-            <Input 
-                placeholder='Example : Satyam@abc.com'
-                onChange={(e)=>{setEmail(e.target.value)}}
-            />
-        </FormControl>
-        <FormControl id='password' isRequired="true">
-            <FormLabel>Password</FormLabel>
-            <Input 
-                type='password'
-                placeholder='Set Password'
-                onChange={(e)=>{setPassword(e.target.value)}}
-            />  
-        </FormControl>
-        <FormControl id='confirmpassword' isRequired="true">
-            <FormLabel>Confirm Password</FormLabel>
-            <Input 
-                type='password'
-                placeholder='Confirm Password'
-                onChange={(e)=>{setConfirmpassword(e.target.value)}}
-            />  
-        </FormControl>
-        <FormControl id='pic'>
-            <FormLabel>Upload your picture</FormLabel>
-            <Input 
-                type='file'
-                p={1.5}
-                accept="image/*"
-                onChange={(e)=>{postDetails(e.target.file[0])}}
-            />
-        </FormControl>
-        <Button 
-        colorScheme='blue'
-        width='100%'
-        m="15px 0 0 0"
-        borderRadius="lg"
-        onClick={submitHandler}>
-            Submit
-        </Button>
-     </VStack>
-    </div>
-  )
-}
+    <VStack spacing="5px">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email Address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmpassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={picLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
+};
 
-export default SignUp
+export default Signup;
