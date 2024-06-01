502a9ad8c18592a961e0d27055992b0b448c9d31
Satyam204 <satyamshivam366@gmail.com>
2024-06-01 21:43:40 +0530

    search user functionality and authmiddleware for user verification (backend)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { registerUser,authUser } = require("../controller/userController");
+const { registerUser,authUser,allUsers } = require("../controller/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", registerUser);
+router.get("/",protect,allUsers);
+
 router.post('/login',authUser);
 
 module.exports = router;
