229dc02ad86d9676e7caf58f92f3373bb8331675
Satyam204 <satyamshivam366@gmail.com>
2024-06-02 20:39:23 +0530

    fetch chat -Api (backend)

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { accessChat } = require("../controller/chatControllers");
+const { accessChat,fetchChats } = require("../controller/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, accessChat);
+router.get("/", protect, fetchChats);
+
 
 module.exports = router;
