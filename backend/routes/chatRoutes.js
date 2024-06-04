5950233ffc923960601f03376c3779ab7a292398
Satyam204 <satyamshivam366@gmail.com>
2024-06-04 10:38:28 +0530

    group API Completed -(Backend)

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { accessChat,fetchChats } = require("../controller/chatControllers");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  removeFromGroup,
+  addToGroup,
+  renameGroup,
+} = require("../controller/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
 
-const router = express.Router();
+const router = express.Router();                                                        
 
 router.post("/", protect, accessChat);
 router.get("/", protect, fetchChats);
-
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
