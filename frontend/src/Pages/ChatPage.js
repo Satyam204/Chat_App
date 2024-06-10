596975814debd11d8740d54164c792901e6559fc
Satyam204 <satyamshivam366@gmail.com>
2024-06-11 00:52:25 +0530

    chatbox layout -(frontend)

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -3,19 +3,22 @@ import { ChatState } from "../Context/ChatProvider"
 import SideDrawer from '../components/miscellaneous/SideDrawer'
 import MyChats from '../components/MyChats';
 import ChatBox from '../components/ChatBox';
+import { useState } from 'react';
 
 const { Box } = require("@chakra-ui/react")
 
 const ChatPage = () => {
-
+  const [fetchAgain, setFetchAgain] = useState(false);
   const {user} =ChatState();
   
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
       <Box display="flex" justifyContent="space-between" width="100%" height="91.5vh" padding="10px">
-        {user&&<MyChats />}
-        {user&&<ChatBox />}
+      {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
