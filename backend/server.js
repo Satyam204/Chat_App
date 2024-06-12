c6fe33c9bf4e11b8094b475a253fbd4e22a09ce5
Satyam204 <satyamshivam366@gmail.com>
2024-06-12 16:56:59 +0530

    Send and Fetch Messagess API Completed -(Backend)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const fileUpload = require("express-fileupload");
+const messageRoutes =require("./routes/messageRoutes");
 
 const app = express();
 
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message",messageRoutes)
 
 app.use(notFound);
 app.use(errorHandler);
