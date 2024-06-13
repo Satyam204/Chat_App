b71cf4a31318c39cd8f6134d2f8970035e19719c
Satyam204 <satyamshivam366@gmail.com>
2024-06-13 10:25:07 +0530

    deployment (Render)

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const fileUpload = require("express-fileupload");
 const messageRoutes = require("./routes/messageRoutes");
+const path =require("path");
 
 const app = express();
 
@@ -23,6 +24,24 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+//--------deployment--------------------------
+
+const dirname1=path.resolve();
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(dirname1,"frontend/build")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirname1,"frontend","build","index.html"));
+  })
+}
+else{
+  app.get("/",(req,res)=>{
+    res.send("API IS RUNNING");
+  })
+}
+//--------deployment--------------------------
 
 app.use(notFound);
 app.use(errorHandler);
